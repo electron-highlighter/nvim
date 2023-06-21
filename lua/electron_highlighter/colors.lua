8e9793b49a8e8ffe377c7de3d8778ec0b460c4c4
Mike McBride <mike.mcbride@hey.com>
2023-06-21 16:48:39 -0500

    update background colors

--- lua/electron_highlighter/colors.lua
+++ lua/electron_highlighter/colors.lua
@@ -5,8 +5,8 @@ local M = {}
 ---@class Palette
 M.default = {
   none = "NONE",
-  bg = "#141820",
-  bg_dark = "#16161e",
+  bg = "#212836",
+  bg_dark = "#141820",
   bg_highlight = "#292e42",
   terminal_black = "#414868",
   fg = "#a8b5d1",
