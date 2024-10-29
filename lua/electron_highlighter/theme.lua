d795688b3516396acbcacf56c1fd5a6ae94b564a
Mike McBride <mike.mcbride@hey.com>
2024-10-29 11:10:31 -0500

    ...

--- lua/electron_highlighter/theme.lua
+++ lua/electron_highlighter/theme.lua
@@ -256,7 +256,7 @@ function M.setup()
     --- Types
     ["@type.builtin"] = { fg = c.orange },
     ["@field"] = { fg = c.fg }, -- For fields.
-    ["@property"] = { fg = util.blend(c.fg, c.cyan, 0.3) },
+    ["@property"] = { fg = util.blend(c.fg, c.cyan, 0.6) },
 
     --- Identifiers
     ["@variable"] = { fg = c.fg, style = options.styles.variables }, -- Any variable name that does not have another highlight.
